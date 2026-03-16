c5d59883040ef9a0c307329ff2a25032e35a5abd
ntabana21. <https://github.com/ntabana21/musana.git/>
2026-03-16 15:59:35 +0200

    seventh commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {defineConfig} from "eslint/config";
 
-// @ts-expect-errore no error we just ignore it
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
-  },
-})
+  }
+})  // ✅ No extra comma or parenthesis here
